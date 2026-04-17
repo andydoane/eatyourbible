796490f0789ef27f1f702cb238ed41d6e3a37463
andydoane <andy.doane@npbc.us>
2026-04-17 08:42:04 -0400

    Update registry.js

--- pwa/verse/verse_games/registry.js
+++ pwa/verse/verse_games/registry.js
@@ -106,7 +106,7 @@ window.EXTERNAL_VERSE_GAMES = [
       description: "Splat the next correct bouncing goo blob.",
       icon: "🫟",
       launchUrl: "./verse_games/verse_splat/index.html",
-      visibleInCarousel: true,
+      visibleInCarousel: false,
       progressType: "standard",
       modes: ["easy", "medium", "hard"]
     }
