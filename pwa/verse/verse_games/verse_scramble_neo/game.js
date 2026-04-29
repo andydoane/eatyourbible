2f624a14b723ebbc58a30492766f908753d3011a
andydoane <andy.doane@npbc.us>
2026-04-29 15:32:59 -0400

    Update game.js

--- pwa/verse/verse_games/verse_scramble_neo/game.js
+++ pwa/verse/verse_games/verse_scramble_neo/game.js
@@ -97,14 +97,7 @@ const state = {
       .replace(/'/g, "&#39;");
   }
 
-  function shuffle(arr){
-    const copy = arr.slice();
-    for (let i = copy.length - 1; i > 0; i--){
-      const j = Math.floor(Math.random() * (i + 1));
-      [copy[i], copy[j]] = [copy[j], copy[i]];
-    }
-    return copy;
-  }
+const shuffle = window.VerseGameShell.shuffle;
 
   function tokenizeVerse(text){
     return String(text || "").trim().split(/\s+/).filter(Boolean);
