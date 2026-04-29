1879dd8066d14ef9545099daa440ae1da05195ee
andydoane <andy.doane@npbc.us>
2026-04-29 14:00:53 -0400

    Update game.js

--- pwa/verse/verse_games/verse_snake/game.js
+++ pwa/verse/verse_games/verse_snake/game.js
@@ -27,24 +27,9 @@
   const BASE_SNAKE_STYLES = ["default","berry","ocean","sun"];
   const SPECIAL_SNAKE_STYLES = ["rainbow","lava","ice","candy","midnight","mono"];
 
-  const ALL_BIBLE_BOOKS = [
-    "Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth",
-    "1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Esther",
-    "Job","Psalms","Proverbs","Ecclesiastes","Song of Solomon","Isaiah","Jeremiah","Lamentations",
-    "Ezekiel","Daniel","Hosea","Joel","Amos","Obadiah","Jonah","Micah","Nahum","Habakkuk","Zephaniah",
-    "Haggai","Zechariah","Malachi",
-    "Matthew","Mark","Luke","John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians",
-    "Ephesians","Philippians","Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy",
-    "Titus","Philemon","Hebrews","James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"
-  ];
+const ALL_BIBLE_BOOKS = window.VerseGameShell.getBibleBookDecoys();
 
-  const FUNNY_DECOY_WORDS = [
-    "taco","banana","penguin","cupcake","dinosaur","pickle","marshmallow","noodle","waffle","rocket",
-    "jellybean","pancake","popcorn","unicorn","bubble","muffin","otter","kangaroo","scooter","rainbow",
-    "pretzel","monkey","donut","cookie","balloon","zebra","narwhal","kitten","puppy","burrito",
-    "pirate","robot","slipper","backpack","bongo","volcano","watermelon","cheeseburger","toothbrush","snowman",
-    "duckling","hamster","meatball","spaghetti","blueberry","coconut","sundae","firetruck","yo-yo","treasure"
-  ];
+const FUNNY_DECOY_WORDS = window.VerseGameShell.getFunDecoys();
 
   const state = {
     rafId: 0,
