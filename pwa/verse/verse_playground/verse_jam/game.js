e2334241d606d5e2e85415dc888d32c1658d1d49
andydoane <andy.doane@npbc.us>
2026-06-23 07:36:45 -0400

    Add files via upload

--- pwa/verse/verse_playground/verse_jam/game.js
+++ pwa/verse/verse_playground/verse_jam/game.js
@@ -218,7 +218,7 @@
 
     // GO! tone after the 3, 2, 1 countdown.
     // Keep this much lower than introWords if GO feels too loud.
-    goTone: 0.15,
+    goTone: 0.10,
 
     // Intro phrase tones:
     // "tap / the / words / match / my / beat"
