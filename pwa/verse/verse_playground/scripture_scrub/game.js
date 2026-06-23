c27e181feb00b524ceb68bf61ddeebbdf15a34ca
andydoane <andy.doane@npbc.us>
2026-06-23 15:47:45 -0400

    Add files via upload

--- pwa/verse/verse_playground/scripture_scrub/game.js
+++ pwa/verse/verse_playground/scripture_scrub/game.js
@@ -307,7 +307,7 @@
     window.VerseGameShell.renderTitleScreen({
       app,
       title: GAME_TITLE,
-      debugBadge: "SS 2.2",
+      debugBadge: "SS 2.3",
       icon: GAME_ICON,
       helpHtml: helpHtml(),
       helpOverlayId: HELP_OVERLAY_ID,
