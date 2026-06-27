2374b897a2e92001e7756dbccb6e09ef7221872d
andydoane <andy.doane@npbc.us>
2026-06-27 12:07:12 -0400

    Add files via upload

--- pwa/verse/verse_playground/verse_typer/game.js
+++ pwa/verse/verse_playground/verse_typer/game.js
@@ -827,7 +827,7 @@
       app,
       title: GAME_TITLE,
       icon: GAME_ICON,
-      debugBadge: "VT 1.5",
+      debugBadge: "VT 1.6",
       helpHtml: helpHtml(),
       helpOverlayId: HELP_OVERLAY_ID,
       startText: "Start",
